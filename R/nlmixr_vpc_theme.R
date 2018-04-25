8bfb854ab0b00a7f6f0fa6a83ac42f1285f6dc6c
Justin Wilkins <justin.wilkins@occams.com>
2018-04-25 17:42:53 +0200

    Update to remove VPC (now in nlmixr)

---
+++ R/nlmixr_vpc_theme.R
@@ -0,0 +1,30 @@
+#' Default VPC theme for 'xpose.nlmixr'
+#'
+#' @description Default VPC theme for 'xpose.nlmixr'
+#'
+#' @importFrom vpc new_vpc_theme
+#' @importFrom xpose theme_xp_default
+#'
+#' @export
+
+nlmixr_vpc_theme <- vpc::new_vpc_theme(list(obs_color = "#1F4E79",
+                                            obs_size = 1,
+                                            obs_median_color = "#1F4E79",
+                                            obs_median_linetype = "solid",
+                                            obs_median_size = 1,
+                                            obs_alpha = .7,
+                                            obs_shape = 1,
+                                            obs_ci_color = "#1F4E79",
+                                            obs_ci_linetype = "dashed",
+                                            obs_ci_size = .5,
+                                            sim_pi_fill = "#C00000",
+                                            sim_pi_alpha = 0.15,
+                                            sim_pi_color = "#1F4E79",
+                                            sim_pi_linetype = 'dotted',
+                                            sim_pi_size = 1,
+                                            sim_median_fill = "#C00000",
+                                            sim_median_alpha = 0.15,
+                                            sim_median_color = "#1F4E79",
+                                            sim_median_linetype = "dashed",
+                                            sim_median_size = 1,
+                                            bin_separators_color = "#000000" ) )
